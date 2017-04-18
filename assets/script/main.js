9eb5c266744bed83eff074dc8e9c7eac805882db
nymph <xahax@outlook.com>
2017-04-19 02:27:26 +0700

    Update main.js

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,22 +1,16 @@
-// var feed = new Instafeed({
-// 		get: 'user',
-// 		userId: 4152793054,
-// 		accessToken: '1822919008.8c76584.723126dc85974c4eb4196ed945be51bc',
-// 		target: 'instagram',
-// 		resolution: 'standard_resolution',
-// 		after: function() {
-// 			var el = document.getElementById('instagram');
-// 			if (el.classList)
-// 				el.classList.add('show');
-// 			else
-// 				el.className += ' ' + 'show';
-// 		}
-// });
-
 var feed = new Instafeed({
-        get: 'user',
-        userId: 4152793054,
-        clientId: '8c76584baded48849d540602dddc19e4',
-        template: '<a class="animation" href="{{link}}"><img src="{{image}}" /></a>'
-    });
-    feed.run();
+		get: 'user',
+		userId: 4152793054,
+		accessToken: '4054629837.0efbe26.bc142395f83a452f8ae03fe28345789f',
+		target: 'instagram',
+		resolution: 'standard_resolution',
+		after: function() {
+			var el = document.getElementById('instagram');
+			if (el.classList)
+				el.classList.add('show');
+			else
+				el.className += ' ' + 'show';
+		}
+});
+
+feed.run();
